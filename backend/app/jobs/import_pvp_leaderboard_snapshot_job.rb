daa81992763883b11c461f5676b83177e6673d96
alessandro <par_214@hotmail.com>
2025-11-19 15:57:16 -0500

    :sparkles: add talents extraction

--- backend/app/jobs/import_pvp_leaderboard_snapshot_job.rb
+++ backend/app/jobs/import_pvp_leaderboard_snapshot_job.rb
@@ -18,8 +18,10 @@ class ImportPvpLeaderboardSnapshotJob < ApplicationJob
 
       snapshot_time = Time.current
 
-      Array(payload["entries"]).each do |entry|
-        import_entry(entry, leaderboard, region, snapshot_time)
+      Array(payload["entries"]).each do |entry_json|
+        character, entry = import_entry(entry_json, leaderboard, region, snapshot_time)
+
+        enrich_from_profile_file(entry, character)
       end
 
 
@@ -65,6 +67,110 @@ class ImportPvpLeaderboardSnapshotJob < ApplicationJob
     character.faction = faction_enum(entry.dig("faction", "type")) if character.respond_to?(:faction=)
 
     character.save! if character.changed?
+
+    pvp_entry = PvpLeaderboardEntry.create!(
+      pvp_leaderboard: leaderboard,
+      character: character,
+      rank: entry["rank"],
+      rating: entry["rating"],
+      wins: stats["won"],
+      losses: stats["lost"],
+      snapshot_at: snapshot_time,
+      class_id: character.class_id,
+      spec_id: nil,
+      spec: nil,
+      item_level: nil,
+      gear_raw: nil,
+      talents_raw: nil
+    )
+
+    [ character, pvp_entry ]
+  end
+
+  def enrich_from_profile_file(entry, character)
+    equipment_path = File.join(Rails.root.join("tmp", "dumps", "nystinn.json").to_s)
+    talents_path = File.join(Rails.root.join("tmp", "dumps", "nystinn_summary.json").to_s)
+
+    update_data = {}
+
+    if File.exist?(equipment_path)
+      gear = JSON.parse(File.read(equipment_path))
+      equipped_items = gear["equipped_items"] || []
+
+      item_levels = equipped_items.map { |i| i.dig("level", "value") }.compact
+      avg_item_level =
+        item_levels.any? ? (item_levels.sum.to_f / item_levels.size).round : nil
+
+      update_data[:gear_raw]   = equipped_items
+      update_data[:item_level] = avg_item_level
+    end
+
+    if File.exist?(talents_path)
+      talents = JSON.parse(File.read(talents_path))
+
+      specs = talents["specializations"] || []
+
+      active_spec = specs.find do |spec|
+        Array(spec["loadouts"]).any? { |loadout| loadout["is_active"] }
+      end || specs.first
+
+      if active_spec
+        spec_info = active_spec["specialization"] || {}
+        spec_name = spec_info["name"]
+        spec_id   = spec_info["id"]
+
+        pvp_talents = Array(active_spec["pvp_talent_slots"]).map do |slot|
+          talent = slot.dig("selected", "talent")
+          next unless talent
+          {
+            "id"   => talent["id"],
+            "name" => talent["name"]
+          }
+        end.compact
+
+        loadout = Array(active_spec["loadouts"]).find { |l| l["is_active"] } ||
+                  Array(active_spec["loadouts"]).first
+
+        # class talents
+        class_talents = Array(loadout["selected_class_talents"]).map do |t|
+          talent_info = t.dig("tooltip", "talent") || {}
+          {
+            "id"   => talent_info["id"],
+            "name" => talent_info["name"],
+            "rank" => t["rank"]
+          }
+        end
+
+        spec_talents = Array(loadout["selected_spec_talents"]).map do |t|
+          talent_info = t.dig("tooltip", "talent") || {}
+          {
+            "id"   => talent_info["id"],
+            "name" => talent_info["name"],
+            "rank" => t["rank"]
+          }
+        end
+
+        hero_talents = Array(loadout["selected_hero_talents"]).map do |t|
+          talent_info = t.dig("tooltip", "talent") || {}
+          {
+            "id"   => talent_info["id"],
+            "name" => talent_info["name"],
+            "rank" => t["rank"]
+          }
+        end
+
+        update_data[:spec]    = spec_name
+        update_data[:spec_id] = spec_id
+        update_data[:talents_raw] = {
+          "pvp_talents"   => pvp_talents,
+          "class_talents" => class_talents,
+          "spec_talents"  => spec_talents,
+          "hero_talents"  => hero_talents
+        }
+      end
+    end
+
+    entry.update!(update_data) if update_data.any?
   end
 
   def faction_enum(type)
