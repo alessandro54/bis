0e86fc8efdaf0ceb2ebfce14956e8a99ed317385
alessandro <par_214@hotmail.com>
2026-02-27 12:14:50 -0500

    :recycle: isolated rails app on repo

--- frontend/src/lib/wow/classes.ts
+++
@@ -1,5 +0,0 @@
-import { WOW_CLASSES, type WowClassConfig, type WowClassSlug } from "@/config/wow/classes";
-
-export function getWowClassBySlug(slug: WowClassSlug): WowClassConfig | null {
-  return WOW_CLASSES.find((c) => c.slug === slug) ?? null;
-}
