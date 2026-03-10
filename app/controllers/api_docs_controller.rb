64aef5d74d94c69a62de9f39592d4b4bd6e9b038
alessandro <par_214@hotmail.com>
2026-03-09 19:38:46 -0500

    feat: add OpenAPI specification and update API documentation routes

--- app/controllers/api_docs_controller.rb
+++ app/controllers/api_docs_controller.rb
@@ -2,4 +2,8 @@ class ApiDocsController < AdminController
   layout false
 
   def index; end
+
+  def spec
+    render file: Rails.root.join("swagger/v1/openapi.yaml"), content_type: "application/yaml"
+  end
 end
